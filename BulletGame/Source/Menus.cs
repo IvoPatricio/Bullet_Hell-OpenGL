a63f392a629752b847336c6cb068d8a483678fc1
IvoPatricio <snoukefm@gmail.com>
2024-10-12 18:26:41 +0100

    menus_todo_list

---
+++ BulletGame/Source/Menus.cs
@@ -0,0 +1,38 @@
+namespace BulletGame
+{
+    public class MainMenu
+    {
+        //Start
+        //Settings
+        //Credits
+        //Quit
+        //Help
+    }
+    public class PauseMenu
+    {
+        //Resume
+        //Settings
+        //Quit
+    }
+    public class SettingsMenu
+    {
+        //Graphics
+            // Normal / Low-end
+        //resolution
+            //fullscreen / window 
+            // 2560x1440 1920x1080 1600x900 1366x768 1360x768 1280x720 1152x648 1024x576 960x540
+        //controls
+            //Key Rebinder / Mouse sensitivity / Invert Mouse Y-Axis
+        //Audio Settings
+            //Master Volume / Music Volume / Sound Effects Volume
+        //GamePlay Settings
+            //Difficulty Level
+        // Save/Load Settings
+    }
+    public class GameOverMenu
+    {
+        //Restart
+        //Return Main Menu
+        //Credits
+    }
+}

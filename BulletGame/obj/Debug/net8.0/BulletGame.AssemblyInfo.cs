9cba379ade7ad0d745edf412f2d90b6a76e717fa
IvoPatricio <snoukefm@gmail.com>
2024-10-12 18:24:48 +0100

    Menus_to_do_list

--- BulletGame/obj/Debug/net8.0/BulletGame.AssemblyInfo.cs
+++ BulletGame/obj/Debug/net8.0/BulletGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulletGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68cf7853345cd7a61cb27c564df2b1315763dae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5408d33b4ee7a3a21334df13758f59c23304cc79")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulletGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulletGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
